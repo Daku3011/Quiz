b8d95e4ccb24d76654c4cfcef75b23d4b3460ed5
Daku3011 <rdwarkesh1300@gmail.com>
2026-01-29 20:50:27 +0530

    fix:Student and faculty portal api calls

--- student_portal/js/config.js
+++ student_portal/js/config.js
@@ -1,4 +1,4 @@
-const CONFIG = {
+window.CONFIG = {
     // API_BASE_URL: 'http://localhost:9090' // Local Development
     API_BASE_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
         ? 'http://localhost:9090' 
